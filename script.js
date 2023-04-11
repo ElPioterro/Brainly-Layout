7e7278fb49b28894af415a530c3da26c5a3f2ff1
El_Pioterro <piotr.szpinda1@gmail.com>
2023-04-11 21:01:01 +0200

    add new elements

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name
+// @name Brainly-Script
 // @description:pl Darmowe nieskończone wyświetlanie odpowiedzi w brainly.pl
 // @author ElPioterro
 // @match https://brainly.pl/*
@@ -8,17 +8,25 @@
 // @version 1.0.0
 // ==/UserScript==
 
-localStorage.clear();
+(function () {
+  "use strict";
 
-// Kill banners and ads
-// GM_addStyle(
-//   "  .js-react-bottom-banner {    display: none !important;  }  #didomi-host {    display: none !important;  }  "
-// );
+  localStorage.clear();
 
-//Alternative solution
-window.onload = function () {
-  document.getElementsByClassName("brn-expanded-bottom-banner")[0].remove();
-  document.getElementsByClassName("brn-brainly-plus-box")[0].remove();
-  document.getElementsByClassName("brn-fullscreen-toplayer")[0].remove();
-  document.getElementsByClassName("sg-overlay sg-overlay--dark")[0].remove();
-};
+  //   GM_addStyle(
+  //     `.js-react-bottom-banner,#didomi-host,.js-dialog,.brn-expanded-bottom-banner,.brn-brainly-plus-box,.brn-fullscreen-toplayer,.sg-overlay,.sg-overlay--dark {    display: none !important;  }`
+  //   );
+  window.onload = function () {
+    document
+      .getElementsByClassName(
+        `brn-expanded-bottom-banner 
+        brn-brainly-plus-box 
+        brn-fullscreen-toplayer 
+        sg-overlay sg-overlay--dark 
+        js-react-bottom-banner
+        js-dialog`
+      )[0]
+      .remove();
+    document.getElementById("didomi-host")[0].remove();
+  };
+})();
