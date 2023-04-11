186d5a06dba3a7f2f12213d14487c365b03e068c
ElPioterro <101070139+ElPioterro@users.noreply.github.com>
2023-04-11 23:38:04 +0200

    smallfix

--- script.js
+++ script.js
@@ -16,17 +16,16 @@
   //   GM_addStyle(
   //     `.js-react-bottom-banner,#didomi-host,.js-dialog,.brn-expanded-bottom-banner,.brn-brainly-plus-box,.brn-fullscreen-toplayer,.sg-overlay,.sg-overlay--dark {    display: none !important;  }`
   //   );
-  window.onload = function () {
-    document
-      .getElementsByClassName(
-        `brn-expanded-bottom-banner 
-        brn-brainly-plus-box 
-        brn-fullscreen-toplayer 
-        sg-overlay sg-overlay--dark 
-        js-react-bottom-banner
-        js-dialog`
-      )[0]
-      .remove();
-    document.getElementById("didomi-host")[0].remove();
-  };
+  //window.onload = function () {
+    //document.getElementsByClassName(`brn-expanded-bottom-banner brn-brainly-plus-box brn-fullscreen-toplayer sg-overlay sg-overlay--dark js-react-bottom-banner js-dialog`)[0].remove();
+    //document.getElementById("didomi-host")[0].remove();
+  //};
+ 
+  //kill all cookies stuff and annoying banners
+ GM_addStyle(
+    ".js-react-bottom-banner,#didomi-host,.js-dialog,.js-react-app-footer,.js-react-below-answers,.js-react-newest-questions,.AnswerBoxLayout-module__comments--DZU8h  {    display: none !important;  }"
+  );
+
+ //enable scrolling
+ document.body.style.setProperty ("overflow", "scroll", "important");
 })();
