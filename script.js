389472b0f44da48b12ab991df397663821c7f776
El_Pioterro <piotr.szpinda1@gmail.com>
2023-04-11 21:30:33 +0200

    add layout

--- script.js
+++ script.js
@@ -5,28 +5,29 @@
 // @match https://brainly.pl/*
 // @grant unsafeWindow
 // @grant GM_addStyle
+// @require https://code.jquery.com/jquery-3.6.4.min.js
 // @version 1.0.0
 // ==/UserScript==
 
 (function () {
   "use strict";
 
-  localStorage.clear();
+  localStorage.clear(); //simply deletes cookies
 
+  // Kill popup and fixed-footer also footer and unneccesary divs
+  GM_addStyle(
+    ".js-react-bottom-banner,#didomi-host,.js-dialog,.js-react-app-footer,.js-react-below-answers,.js-react-newest-questions,.AnswerBoxLayout-module__comments--DZU8h  {    display: none !important;  }"
+  );
+
+  //kill optional divs
+  GM_addStyle(
+    "[data-testid='textbooks_q_a_entry_point_options_classes_test_id'],.brn-header-container,.brn-qpage-layout__right   {    display: none !important;  }"
+  );
+  //fix for scrollbar
+  GM_addStyle(".sg-dialog-no-scroll{position: relative;}");
+
+  //even add grid
   //   GM_addStyle(
-  //     `.js-react-bottom-banner,#didomi-host,.js-dialog,.brn-expanded-bottom-banner,.brn-brainly-plus-box,.brn-fullscreen-toplayer,.sg-overlay,.sg-overlay--dark {    display: none !important;  }`
+  //     ".brn-qpage-layout {    grid-template-areas:        'main main-bottom'        'main main-bottom';     grid-template-columns: 50%; .brn-qpage-bottom-section { margin-top:0 }"
   //   );
-  window.onload = function () {
-    document
-      .getElementsByClassName(
-        `brn-expanded-bottom-banner 
-        brn-brainly-plus-box 
-        brn-fullscreen-toplayer 
-        sg-overlay sg-overlay--dark 
-        js-react-bottom-banner
-        js-dialog`
-      )[0]
-      .remove();
-    document.getElementById("didomi-host")[0].remove();
-  };
 })();
